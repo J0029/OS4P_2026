1277590d89adeb1584d8f259147830aa31a2f25c
J0029 <j.murphy@students.uu.nl>
2026-09-16 01:17:04 +0200

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,7 +1,7 @@
 ------------ 
 # Simulation of a falling body on Earth 
-ACCELERATION_G = 10  # m/s^2 
-TIME = 9.80665              # seconds 
+ACCELERATION_G = 9.80665  # m/s^2 
+TIME = 5              # seconds 
 def calculate_displacement(g, t): 
   return 0.5 * g * (t ** 2) 
 print(f"Displacement after {TIME}s: {calculate_displacement(ACCELERATION_G, TIME)} meters") 
